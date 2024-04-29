1364e09ecbc5879be055b2b947972047c6a54864
henek93 <sludnikov.slava03@gmail.com>
2024-04-29 13:18:29 +0300

    добавил mapper и databaseRepositoryImpl

--- app/src/main/java/com/example/movieapp/data/repositoryImpl/DatabaseRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/data/repositoryImpl/DatabaseRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package com.example.movieapp.data.repositoryImpl
 
-class DatabaseRepositoryImpl {
+import android.app.Application
+import androidx.lifecycle.MediatorLiveData
+import com.example.movieapp.data.database.MovieDatabase
+import com.example.movieapp.data.mapper.DbMapper
+import com.example.movieapp.domain.dataBaseRepository.DatabaseRepository
+import com.example.movieapp.domain.enteties.Movie
+
+class DatabaseRepositoryImpl : DatabaseRepository {
+
+    private val movieDao = MovieDatabase.getInstance(Application()).movieDao()
+    private val mapper = DbMapper()
+
+    override fun getListFavouriteMovie() = MediatorLiveData<List<Movie>>().apply {
+        addSource(movieDao.getListMovieFromDb()) {
+            value = mapper.mapListDbMovieToListMovie(it)
+        }
+    }
+
+    override suspend fun getMovie(movieId: Int) =
+        mapper.mapDbMovieToMovie(movieDao.getMovieFromDb(movieId))
+
+    override suspend fun deleteMovieFromDb(movieId: Int) {
+        movieDao.deleteMovieFromDb(movieId)
+    }
+
+    override suspend fun addMovieToDb(movie: Movie) {
+        movieDao.addMovieToDb(mapper.mapMovieToDbMovie(movie))
+    }
+
+
 }
